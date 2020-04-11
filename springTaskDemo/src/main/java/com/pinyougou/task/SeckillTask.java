9123aaad1501bcde0e57c6b8e32d0aa17db7ffae
quanbinchen <1187214376@qq.com>
2020-04-11 18:30:38 +0800

    springTask测试第一次提交

---
+++ springTaskDemo/src/main/java/com/pinyougou/task/SeckillTask.java
@@ -0,0 +1,25 @@
+package com.pinyougou.task;
+
+import java.util.Date;
+
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+/**
+ * 秒杀组件
+ * 
+ * @author cqb
+ *
+ */
+@Component
+public class SeckillTask {
+
+	/**
+	 * 刷新秒杀商品
+	 */
+	@Scheduled(cron="12/4 * * * * ?")
+	public void refreshSeckillGoods(){
+		System.out.println("执行了任务调度"+new Date());		
+	}		
+
+}
